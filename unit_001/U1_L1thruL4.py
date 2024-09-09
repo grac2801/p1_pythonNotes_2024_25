d0724255c6ec5f5c0e8368a0a5db5cb43bae6298
grac2801 <oscar.gracias@icloud.com>
2024-09-09 12:01:23 -0700

    Starter code unit 1 print()

---
+++ unit_001/U1_L1thruL4.py
@@ -0,0 +1,41 @@
+'''
+Created on Sep 9, 2024
+
+@author: ogracias
+'''
+print('Hello world')
+
+# default separator
+print('Hello', 'Mr.','Gracias')
+print('Hello world')
+
+# explicit separator
+print('Hello', 'Mr.', 'Gracias', sep='&')
+print('Hello', 'Mr.', 'Gracias', sep='\n')
+
+firstNumber = 5
+secondNumber = 10
+
+print(firstNumber)
+print(secondNumber)
+
+print(firstNumber, end=' and ')
+print(secondNumber)
+
+# format
+myAge = 53
+favoriteColor = 'dark blue'
+print('I am {0} years old, and my favorite color is {1}.'.format(myAge, favoriteColor))
+print('I am {0} years old, and my favorite color is {1}.'.format(favoriteColor, myAge))
+print('I am {1} years old, and my favorite color is {0}.'.format(favoriteColor, myAge))
+
+
+#new formatting
+print(f'I am {myAge} years old, and my favorite color is {favoriteColor}.')
+
+
+
+
+
+
+
