ad8d2a62b364320fb570f34f5e1054079b75d04e
grac2801 <oscar.gracias@icloud.com>
2024-09-13 10:30:02 -0700

    updated input and data types U1_L5 and L6

---
+++ unit_001/U1_L7_dataTypes.py
@@ -0,0 +1,41 @@
+'''
+2024-09-13
+ogracias
+title = data types
+'''
+print('\n\n********************')
+print('Adding 2 numbers')
+print('********************')
+num1 = input('Enter a number: ')
+num2 = input('Enter another number: ')
+print(num1 + num2)
+
+
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+print(num1 + num2)
+
+
+print('\n\n********************')
+print('entering a number and printing it')
+print('********************')
+number = int(input('Enter a number: '))
+print('You entered: ' + str(number))#cast into a string
+print('You entered', number)
+
+
+print('\n\n********************')
+print('floats')
+print('********************')
+dec1 = float(input('Enter a decimal number: '))
+dec2 = float(input('Enter another decimal number: '))
+total = dec1 + dec2
+print('My total is: ' + str(total))
+print('My total is', total)
+print(type(total))
+
+
+
+
+
+
