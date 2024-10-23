d4bb2b61761f2c13cd8fc7a7c169ce5286867380
grac2801 <oscar.gracias@icloud.com>
2024-10-23 08:58:43 -0700

    U4_L7 for loops

---
+++ unit_004_iteration/U4_L7_ForLoops.py
@@ -0,0 +1,60 @@
+'''
+2024-10-23
+ogracias
+topic = for loops
+'''
+
+# imports
+#
+#
+# print('\n\n********************')
+# print('#1 review of while')
+# print('********************')
+# '''
+# You need 3 things: 
+# 1-declare and initialize
+# 2-test (T or F)
+# 3-change the variable
+# '''
+# count = 1
+# while(count < 6):
+#     print(str(count) + ' : Hello')
+#     count += 1
+#
+# print('\n\n********************')
+# print('#2- syntax of a for loop')
+# print('********************')
+# for each in range(5):
+#     print('Hello')
+#
+# print('\n\n********************')
+# print('#3- range(start, stop)')
+# print('********************')
+# for i in range(1, 11):
+#     print(i, end=" ")
+#
+# print('\n\n********************')
+# print('#4- range(start, stop, step)')
+# print('********************')
+# n = int(input('How many iterations? '))
+# for i in range(2, n + 1, 2):
+#     print(i)
+    
+    
+print('\n\n********************')
+print('#5- range(start, stop, step)')
+print('********************')
+for i in range(100, 0, -10):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
